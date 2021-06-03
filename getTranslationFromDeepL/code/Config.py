2dc273cb387d807ae0f7507d6dc2ac7b0f3add89
Yuyixiong-123 <1544570116@qq.com>
2021-06-03 22:23:46 +0800

    sd

--- getTranslationFromDeepL/code/Config.py
+++ getTranslationFromDeepL/code/Config.py
@@ -6,6 +6,6 @@ Created on Fri Mar 19 11:24:42 2021
 
 note: 
 """
-paraElementsPath="../paraNeedTranslate/paragraphElement-phase3.xlsx"
+paraElementsPath="../paraNeedTranslate/paragraphElement-phase4.2.xlsx"
 templatePath="../outputData/template.docx"
-savePath="../outputData/paragraphElement-phase3.docx"
+savePath="../outputData/paragraphElement-phase4.docx"
